687f1685501a4063db828ce9033cc663278d80ec
xiaox <xiaox@gmail.com>
2024-03-28 15:30:32 +0800

    feat: 支持网络图片压缩

--- src/tinypng.js
+++ src/tinypng.js
@@ -12,7 +12,10 @@ const Utf8 = require('crypto-js/enc-utf8')
  * @returns {Promise<Buffer>}
  */
 async function tinypngKeyCompress(ctx, imageUrl, key) {
-  return await fs.readFile(imageUrl).then(fileData => {
+
+  const fileDataPromise = imageUrl.startsWith("http") ? utils.fetchImage(ctx, imageUrl) : fs.readFile(imageUrl);
+
+  return await fileDataPromise.then(fileData => {
     // 1.构造请求参数（向tinypng请求）格式参考https://tinify.cn/developers/reference
     const bearer = Base64.stringify(Utf8.parse(`api:${key}`))
     const fetchOptions = {
@@ -26,18 +29,20 @@ async function tinypngKeyCompress(ctx, imageUrl, key) {
       },
       data: fileData
     }
-
     // 2.发送压缩请求
     return ctx.request(fetchOptions).then(response => {
       // 3.获取压缩后图片的url
       if (response.status && response.status >= 200 && response.status <= 299) {
         const location = response.headers.location
         ctx.log.info('压缩后的地址：', location)
-        
+
         // 4.从url中获取网络图片
         return utils.fetchImage(ctx, location).then(res => {
           // 5.获取压缩后的图片buffer，构造picGO需要的格式
-          return utils.getImageInfo(imageUrl, res.data)
+          return {
+            input: location, // 地址变成压缩后的地址了.
+            output: utils.getImageInfo(imageUrl, res.data)
+          }
         })
       }
       throw new Error('压缩出错')
